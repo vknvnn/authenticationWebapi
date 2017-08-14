a32d367587a3c77634eec3930ba419e87bd65891
NghiepVo <NghiepVo@NGHIEPVO>
2017-08-14 14:04:15 +0700

    new Source

---
+++ AngularJSAuthentication-master/Client/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace Client
+{
+    class Program
+    {
+        const string userName = "vknvnn";
+        const string password = "vknvnn";
+        const string apiAuthentication = "http://localhost:26264/";
+        private const string apiResource = "http://localhost:47039/";
+        const string apiGetPeoplePath = "/api/people";
+
+        static void Main(string[] args)
+        {
+            //Get the token
+            var token = GetAPIToken(userName, password, apiAuthentication).Result;
+            Console.WriteLine("Token: {0}", token);
+
+            ////Make the call
+            //var response = GetRequest(token, apiAuthentication, apiGetPeoplePath).Result;
+            //Console.WriteLine("response: {0}", response);
+
+            //wait for key press to exit
+            Console.ReadKey();
+        }
+
+        private static async Task<string> GetAPIToken(string userName, string password, string apiBaseUri)
+        {
+            using (var client = new HttpClient())
+            {
+                //setup client
+                client.BaseAddress = new Uri(apiBaseUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //setup login data
+                var formContent = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("client_id", "consoleApp"),
+                    new KeyValuePair<string, string>("grant_type", "password"),
+                    new KeyValuePair<string, string>("username", userName),
+                    new KeyValuePair<string, string>("password", password),
+                });
+
+                //send request
+                HttpResponseMessage responseMessage = await client.PostAsync("/Token", formContent);
+
+                //get access token from response body
+                var responseJson = await responseMessage.Content.ReadAsStringAsync();
+                var jObject = JObject.Parse(responseJson);
+                return jObject.ToString();
+            }
+        }
+
+        static async Task<string> GetRequest(string token, string apiBaseUri, string requestPath)
+        {
+            using (var client = new HttpClient())
+            {
+                //setup client
+                client.BaseAddress = new Uri(apiBaseUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
+
+                //make request
+                HttpResponseMessage response = await client.GetAsync(requestPath);
+                var responseString = await response.Content.ReadAsStringAsync();
+                return responseString;
+            }
+        }
+    }
+}
